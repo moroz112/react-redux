dcbb31655f02edcd0b3fafc73d4bd702dd554031
amoroz <morozalexrpd@gmail.com>
2017-08-27 16:02:32 +0300

    try rxjs

--- try-rxjs/app.js
+++ try-rxjs/app.js
@@ -1,20 +1,28 @@
-import Rx from 'rxjs/Rx';
+var button = document.querySelector('button');
+var label = document.querySelector('h4');
 
-var observable = Rx.Observable.create(function (observer) {
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
-    setTimeout(() => {
-        observer.next(4);
-        observer.complete();
-    }, 1000);
+var clickStream = Rx.Observable.fromEvent(button, 'click');
+console.log('clickStream', clickStream);
+
+var doubleClickStream = clickStream
+    .bufferWhen(() => clickStream.debounceTime(250))
+    .map(arr => arr.length)
+    .filter(len => len === 2);
+
+console.log(doubleClickStream);
+doubleClickStream.subscribe(event => {
+    label.textContent = 'double click';
+    console.log('event', event);
 });
 
+doubleClickStream
+    .delay(1000)
+    .subscribe(suggestion => {
+        label.textContent = '-';
+    });
 
-console.log('just before subscribe');
-observable.subscribe({
-    next: x => console.log('got value ' + x),
-    error: err => console.error('something wrong occurred: ' + err),
-    complete: () => console.log('done'),
-});
+var streamA = Rx.Observable.of(3,5);
+console.log(streamA);
 
+var streamB = streamA.map(a => a * 10);
+streamB.subscribe(b => console.log('b', b));
